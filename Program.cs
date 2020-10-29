ed9e8a43fc060f9575f760191d66a0a43e35ca22
Krystal <stinson.krystal327@gmail.com>
2020-10-29 12:00:58 -0400

    first commit for capstone

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Vacation Budget Planner!");
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hi {name}! Where would you like to travel: Portugal or Brazil?");
+            string travelLocation = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
